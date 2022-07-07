06ab4952dc24ce2ff7ec7cf984cca73cf2314bac
Julian Mojico <jmojico@gmail.com>
2022-07-07 04:33:16 -0300

    Social icons improvement

--- src/components/SocialNetworks.js
+++ src/components/SocialNetworks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { IconContext } from "react-icons";
-import { FaInstagram, FaTwitter, FaFacebook, FaYoutube, FaPinterest, FaTiktok, FaTwitch, FaReddit, FaGithub } from 'react-icons/fa';
+import icons from "../utils/icons";
 
 const SocialNetworks = ({ data, ...props }) => {
 
@@ -8,21 +8,9 @@ const SocialNetworks = ({ data, ...props }) => {
 
     return <IconContext.Provider value={{ color: "gray", className: "global-class-name" }}>
         <div  {...props} >
-            <ul className="d-flex justify-content-around list-unstyled">
-                {socialNetworks.map(site => {
-                    switch (site) {
-                        case 'instagram': return <li><FaInstagram /></li>; 
-                        case 'twitter':  return <li><FaTwitter /></li>;
-                        case 'facebook': return <li><FaFacebook /></li>;
-                        case 'youtube': return <li><FaYoutube /></li>;
-                        case 'pinterest': return <li><FaPinterest /></li>;
-                        case 'tiktok': return <li><FaTiktok /></li>;
-                        case 'twitch': return <li><FaTwitch /></li>;
-                        case 'reddit': return <li><FaReddit /></li>;
-                        case 'github': return <li><FaGithub /></li>;
-                        default:
-                    }
-                })}
+            <ul className="d-flex justify-content-between justify-content-md-start  list-unstyled">
+                {socialNetworks.map(site => <li className='mx-2'>{icons[site]}</li>
+                )}
             </ul>
         </div>
     </IconContext.Provider>
---
+++ src/utils/icons.js
@@ -0,0 +1,15 @@
+import { FaInstagram, FaTwitter, FaFacebook, FaYoutube, FaPinterest, FaTiktok, FaTwitch, FaReddit, FaGithub } from 'react-icons/fa';
+
+const icons = {
+    'instagram': <FaInstagram />,
+    'twitter': <FaTwitter />,
+    'facebook':<FaFacebook />,
+    'youtube':<FaYoutube />,
+    'pinterest':<FaPinterest />,
+    'tiktok':<FaTiktok />,
+    'twitch':<FaTwitch />,
+    'reddit':<FaReddit />,
+    'github':<FaGithub />,
+}
+
+export default icons;
